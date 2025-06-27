ff0775e08c92fe359045639910418a89329bbfae
Tharun Kumar <tharun.tiruppali@gmail.com>
2025-06-27 00:01:12 -0700

    Initial Commit

---
+++ chillers/chillersApp.swift
@@ -0,0 +1,17 @@
+//
+//  chillersApp.swift
+//  chillers
+//
+//  Created by Tharun Kumar on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct chillersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
